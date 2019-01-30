9a0aac145b2b5f5614b999f5e310665a268ce277
nichikayoshida <nichika.yoshida@gmail.com>
2019-01-31 00:22:28 +0900

    initial commit

---
+++ app/src/main/java/com/example/nichiyoshi/databindingpractice/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.nichiyoshi.databindingpractice
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.example.nichiyoshi.databindingpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding : ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val user = User("Test", "User")
+        binding.user = user
+        binding.listener = this
+    }
+
+    override fun onClick(view: View?) {
+        if(view == binding.button){
+            (view as Button).let{
+                it.text = "clicked!!!"
+            }
+        }
+    }
+}
